428f25aff85544f7d28ffe72ac33fff95bd0a492
Hogyun Jeon <akzm9999@gmail.com>
2024-11-05 15:44:47 +0900

    chore: 루트 prettier-eslint 충돌하지 않도록 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import prettierPluginRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
+  prettierPluginRecommended,
 ];
