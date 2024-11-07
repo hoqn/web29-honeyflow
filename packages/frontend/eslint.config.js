dd4e9f57a64ef81747c9d9687ad9d84948a256dd
Heejin Na <105726712+heegenie@users.noreply.github.com>
2024-11-07 16:48:10 +0900

    chore: 스타일 라이브러리 설정 (#80)
    * chore: tailwind css 관련 패키지 설치 및 초기 설정
    
    * chore: 유틸리티 클래스 자동 정렬 플러그인 추가
    
    * chore: eslint.config.js 파일로 `eslint-plugin-storybook` 설정 이동
    
    * chore: path alias 관련 설정 추가
    
    * chore: import 정렬 플러그인 설정
    
    * chore: `shadcn/ui` 라이브러리 설정
    
    * style: `tailwindcss-animate` 플러그인을 es6 모듈로 import하도록 수정

--- packages/frontend/eslint.config.js
+++ packages/frontend/eslint.config.js
@@ -3,13 +3,19 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import storybook from "eslint-plugin-storybook";
 
 import rootEsLint from "../../eslint.config.mjs";
 
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...rootEsLint],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...rootEsLint,
+      ...storybook.configs["flat/recommended"],
+    ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,7 +30,10 @@ export default tseslint.config(
       "no-shadow": "off",
       "import/no-absolute-path": "warn",
       "import/no-unresolved": "warn",
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
-  }
+  },
 );
