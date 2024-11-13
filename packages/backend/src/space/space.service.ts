1eca120807a0e3551eb2c40e2387d3388688af7f
fru1tworld <fruitworld.planet@gmail.com>
2024-11-13 20:40:41 +0900

    chore: 상세 비즈니스 로직에 대한 주석처리

--- packages/backend/src/space/space.service.ts
+++ packages/backend/src/space/space.service.ts
@@ -22,6 +22,10 @@ export class SpaceService {
     const Nodes: Node[] = [];
 
     const spaceId = this.generateUuid();
+    //Todo 1명의 User는 unique한 spacename만 생성할 수 있어야한다.
+    //Todo 1명의 User는 최대 MAX_SPACES개의 space를 생성할 수 있어야한다.
+    //Todo 로그인이 기능이 없는 경우 UserId는 honeyflow가 유일하다.
+    //Todo 위 사항은 회의를 통해 논의되어야한다.
     const result = await this.spaceRepository.save({
       userId: userId,
       spaceId: spaceId,
