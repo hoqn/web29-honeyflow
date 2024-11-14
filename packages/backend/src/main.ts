1aed470259100cbf494e83600adb0911fa15bdfb
fru1tworld <fruitworld.planet@gmail.com>
2024-11-14 13:03:01 +0900

    fix: pr리뷰 반영

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
-
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.useGlobalFilters(new AllExceptionsFilter());
   app.enableCors({
     origin: ['http://www.honeyflow.life', 'https://www.honeyflow.life'],
     methods: 'GET, POST, PUT, DELETE',
--- packages/backend/src/space/space.controller.ts
+++ packages/backend/src/space/space.controller.ts
@@ -2,41 +2,44 @@ import {
   Body,
   Controller,
   Get,
-  HttpCode,
   HttpException,
   HttpStatus,
   Param,
   Post,
-  Res,
   Version,
 } from '@nestjs/common';
 import { SpaceService } from './space.service';
 import { CreateSpaceDto } from './dto/create.space.dto';
-import { Response } from 'express';
 @Controller('space')
 export class SpaceController {
   constructor(private readonly SpaceService: SpaceService) {}
 
   @Version('1')
   @Post()
-  async createSpace(
-    @Body() createSpaceDto: CreateSpaceDto,
-    @Res() res: Response,
-  ) {
+  async createSpace(@Body() createSpaceDto: CreateSpaceDto) {
     const { userId, spaceName } = createSpaceDto;
 
-    if (!userId || !spaceName) {
-      return res.status(400).json({ message: '필요한 정보가 누락되었습니다.' });
-    }
-
     const space = await this.SpaceService.create(userId, spaceName);
-    return res.status(201).json({ spaceId: space.spaceId });
+    if (!space) {
+      throw new HttpException('스페이스 생성 실패', HttpStatus.BAD_REQUEST);
+    }
+    return {
+      spaceId: space.spaceId,
+    };
   }
 
   @Version('1')
   @Get('v1/:spaceId')
-  async getSpace(@Param('spaceId') spaceId: string, @Res() res: Response) {
+  async getSpace(@Param('spaceId') spaceId: string) {
     const space = await this.SpaceService.findById(spaceId);
-    return res.status(200).json({ space });
+    if (!space) {
+      throw new HttpException(
+        '스페이스가 존재하지 않습니다.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return {
+      space,
+    };
   }
 }
