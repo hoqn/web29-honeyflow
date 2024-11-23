f91be3b7b0cef3a897254c6f90cbe37e50888894
Hogyun Jeon <akzm9999@gmail.com>
2024-11-23 14:45:23 +0900

    fix: fe space 웹소켓 연결 주소 수정

--- packages/frontend/src/hooks/yjs/useYjsConnection.tsx
+++ packages/frontend/src/hooks/yjs/useYjsConnection.tsx
@@ -9,7 +9,6 @@ export default function useYjsConnection(docName: string) {
 
   useEffect(() => {
     const doc = new Y.Doc();
-    // FIXME: backend 서버로 변경
     const provider = new WebsocketProvider(
       // `ws://${import.meta.env.DEV ? "localhost" : "www.honeyflow.life"}/space`,
       `ws://${"localhost"}/space`,
